b24276cc5c15d500affa7e1ea6686720cd54072b
Hamayun Saeed <hamayunsaeed48@gmail.com>
2026-07-21 08:32:18 -0700

    polish portfolio fix some responsive issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2092149453" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2938510375" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
